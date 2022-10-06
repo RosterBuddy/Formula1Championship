611bc6b740cd8d2fe1e52ad47cd9e8aa07abc71d
mrbreenhd <eliteracerYT@gmail.com>
2022-10-06 23:19:38 +0100

    checks if driver is AI or not

--- app/Models/Drivers.php
+++ app/Models/Drivers.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Drivers extends Model
 {
     use HasFactory;
+
+    protected $table = 'drivers';
+
+    protected $fillable = [ 'name', 'team', 'user_id'];
+
+    public function real_checker()
+    {
+        if($this->user_id == 1){
+            return "Real";
+        }else{
+            return "AI";
+        }
+    }
 }
--- resources/views/admin/drivers/overview.blade.php
+++ resources/views/admin/drivers/overview.blade.php
@@ -5,25 +5,23 @@
             <h5 class="card-title">
                 <div class="row">
                     <div class="col">Manage Drivers</div>
-                    <div class="col text-right"><a class="btn btn-primary" href="{{route('admin.create_race')}}">Add a driver</a></div>
+                    <div class="col text-right"><a class="btn btn-primary" href="{{route('admin.drivers_create')}}">Add a driver</a></div>
             </div>
             </h5>
             <table class="text-center table table-hover">
                 <thead>
                 <tr>
-                    <th scope="col">#</th>
                     <th scope="col">Name</th>
                     <th scope="col">Team</th>            
-                    <th scope="col">User ID</th>
+                    <th scope="col">Type</th>
                 </tr>
                 </thead>
                 <tbody>
                     @foreach($drivers as $driver)
                         <tr>
-                            <th scope="row">{{$driver->id}}</th>
                             <td>{{$driver->name}}</td>
                             <td>{{$driver->team}}</td>                    
-                            <td>{{$driver->user_id}}</td>
+                            <td>{{$driver->real_checker()}}</td>
                         </tr>
                     @endforeach
                 </tbody>
