5597396741fc010c3628b49c57f5a8782349f451
mrbreenhd <eliteracerYT@gmail.com>
2022-10-06 22:41:08 +0100

    Moved to folders s

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,12 +12,12 @@ class AdminController extends Controller
     public function manage_races()
     {
         $races = Race::orderBy('id', 'desc')->get();
-        return view('admin.view_races', compact('races'));
+        return view('admin.race.view_races', compact('races'));
     }
 
     public function create_race()
     {
-        return view('admin.create_race');
+        return view('admin.race.create_race');
     }
 
     public function store_race(Request $request)
@@ -29,12 +29,12 @@ class AdminController extends Controller
             'active' => 0,
         ]);
         notify()->success('Welcome to Laravel Notify ⚡️');
-        return redirect(route('admin.create_race'));
+        return redirect(route('admin.race.create_race'));
     }
 
     public function show_race($id)
     {
         $race = Race::find($id);
-        return view('admin.show_race', compact('race'));
+        return view('admin.race.show_race', compact('race'));
     }
 }
