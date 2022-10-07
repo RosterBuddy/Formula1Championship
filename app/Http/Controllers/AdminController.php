444d75ed9bea18988ed3db68b2d00a051ea69ee5
mrbreenhd <eliteracerYT@gmail.com>
2022-10-07 12:31:28 +0100

    Assigns points based on FL + position

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -54,15 +54,31 @@ class AdminController extends Controller
     public function insert_race_results(Request $request)
     {
         $checked = 0;
-        
+        $points = 0;
+
         if (isset($request->fastest_lap)) {
             $checked = 1;
         }
+
+        if($request->position == 1) $points = 25;
+        if($request->position == 2) $points = 18;
+        if($request->position == 3) $points = 15;
+        if($request->position == 4) $points = 12;
+        if($request->position == 5) $points = 10;
+        if($request->position == 6) $points = 8;
+        if($request->position == 7) $points = 6;
+        if($request->position == 8) $points = 4;
+        if($request->position == 9) $points = 2;
+        if($request->position == 10) $points = 1;
+
+        if($request->position <= 10 && $checked == 1) $points++;
+
         $result = Results::create([
             'race_id' => $request->race_id,
             'driver_id' => $request->driver,
             'position' => $request->position,
             'fastest_lap' => $checked,
+            'points' => $points,
         ]);
 
         notify()->success('Result Sucessfully Saved');
