67613841c8ff42564dc2267bd2fb06fa7c1d8357
mrbreenhd <eliteracerYT@gmail.com>
2022-10-12 14:13:19 +0100

    Renamed commands and adds teams

--- app/Console/Commands/Standings.php
+++ app/Console/Commands/Standings.php
@@ -4,18 +4,20 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\DriverStandings;
+use App\Models\TeamStandings;
+use App\Models\Teams;
 use App\Models\Drivers;
 use App\Models\Results;
 use DB;
 
-class driver_standings extends Command
+class standings extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'command:driverstandings';
+    protected $signature = 'command:standings';
 
     /**
      * The console command description.
@@ -42,6 +44,21 @@ class driver_standings extends Command
             $driverstanding = DriverStandings::updateOrCreate(
                 ['driver_id' => $result->driver_id],
                 ['points' => $result->points]
+            );
+            $driverstanding->save();
+        }
+
+        $team_standings = DB::table('race_results')
+        ->select('team_id', DB::raw('sum(points) AS points'))
+        ->groupBy('team_id')
+        ->get();
+
+        $team_standing = json_decode(json_encode($team_standings), true);
+
+        foreach($team_standings as $team){            
+            $driverstanding = TeamStandings::updateOrCreate(
+                ['team_id' => $team->team_id],
+                ['points' => $team->points]
             );
             $driverstanding->save();
         }
