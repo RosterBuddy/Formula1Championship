507ec0811cf3c3ca510c52a2c0e7b7d30c9435af
mrbreenhd <eliteracerYT@gmail.com>
2022-10-12 16:19:33 +0100

    Changes to Integer

--- database/migrations/2022_10_06_214654_drivers.php
+++ database/migrations/2022_10_06_214654_drivers.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('drivers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('team');
+            $table->integer('team');
             $table->integer('user_id')->nullable();
             $table->timestamps();
         });
